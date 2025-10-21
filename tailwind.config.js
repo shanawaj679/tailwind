e8558aa952f0c0c04337f7cf7c964af7817e75f2
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2025-10-21 18:57:15 +0530

    nothing more just learning commit in git

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"], // tell Tailwind where to scan for classes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
